5fc1b3c6c48b946b713e080132fcbeccbbd559b1
Sarah Kaminski <sarahe.kaminski@gmail.com>
2017-02-28 12:21:25 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "My favorite color is Purple";
+
+            Console.WriteLine(string.Join(" ", sentence.Split(' ').Reverse()));
+        }
+    }
+}
